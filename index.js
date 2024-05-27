4511732fa71927dd64208f0d32225c0bf43f8d88
Ayanabha Misra <misrarimbo@gmail.com>
2024-05-27 13:45:37 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv/config");
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send({ message: "Hello ji this is server 1" });
+});
+app.get("/service1/hello", (req, res) => {
+  res.send({ message: "Hello ji this is server 1" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server 1 is running on port", PORT);
+});
